f2d284e347d298218cf7f3a395f352c1b66b967d
Anat <Anat.md@gmail.com>
2018-10-23 12:00:39 +0300

    4th commit search bar and model draft for gimages

--- js/util.js
+++ js/util.js
@@ -1,3 +1,20 @@
 'use strict'
 
+//model - CRUDL for gImages
+
 var gFontSize = '20px';
+var gImages = [
+    {
+        id: 1,
+        src: 'lala.jpg' ,
+        desc: 'angry child'
+    }
+]
+
+function searchImage() {
+
+}
+
+function getImageById() {
+
+}
